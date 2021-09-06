69edf21b420695605627576b4d6c1932f1bea221
EnderTheCoder <1991455223@qq.com>
2021-09-07 00:05:50 +0800

    重写了计时器，增加了多班级课程表切换的功能，便于未来兼容全年级使用

--- js/classListController.js
+++ js/classListController.js
@@ -1,29 +1,51 @@
-let timePreFix = 60;
+let timePreFix = 2;
 let classStartSchedule =
-    [[5,40],[7,0],[8,0],[8,55],[10,10],[11,5],[14,15],[15,5],[15,55],[16,45],[17,35],[18,40],[19,55],[21,0]];
+    [[5, 40], [7, 0], [8, 0], [8, 55], [10, 10], [11, 5], [14, 15], [15, 5], [15, 55], [16, 45], [17, 35], [18, 40], [19, 55], [21, 0]];
 let classEndSchedule =
-    [[6,30],[7,50],[8,45],[9,40],[10,55],[11,50],[14,55],[15,45],[16,35],[17,25],[18,10],[19,45],[20,50],[21,50]];
+    [[6, 30], [7, 50], [8, 45], [9, 40], [10, 55], [11, 50], [14, 55], [15, 45], [16, 35], [17, 25], [18, 10], [19, 45], [20, 50], [21, 50]];
+
 let classList = [
-    [['早读'],['自习'],['物理'],['物理'],['化学'],['化学'],['语文'],['语文'],['语文'],['语文'],['自习'],['数学'],['物理'],['语文']],
-    [['早读'],['生物'],['数学'],['数学'],['物理'],['语文'],['英语'],['化学'],['生物'],['英语'],['化学'],['数学'],['物理'],['语文']],
-    [['早读'],['化学'],['物理'],['英语'],['语文'],['语文'],['生物'],['化学'],['数学'],['英语'],['生物'],['数学'],['物理'],['语文']],
-    [['早读'],['生物'],['数学'],['数学'],['语文'],['英语'],['物理'],['生物'],['化学'],['英语'],['化学'],['数学'],['物理'],['语文']],
-    [['早读'],['化学'],['语文'],['英语'],['数学'],['数学'],['化学'],['物理'],['生物'],['英语'],['生物'],['数学'],['物理'],['语文']],
-    [['早读'],['生物'],['数学'],['物理'],['语文'],['英语'],['化学'],['生物'],['英语'],['英语'],['化学'],['数学'],['物理'],['语文']],
-    [['早读'],['化学'],['生物'],['生物'],['英语'],['英语'],['数学'],['数学'],['数学'],['数学'],['生物'],['数学'],['物理'],['语文']],
+    [],//一班
+    [],//二班
+    [],//三班
+    [],//四班
+    [
+        [['早读'], ['自习'], ['物理'], ['物理'], ['化学'], ['化学'], ['语文'], ['语文'], ['语文'], ['语文'], ['自习'], ['数学'], ['物理'], ['语文']],
+        [['早读'], ['生物'], ['数学'], ['数学'], ['物理'], ['语文'], ['英语'], ['化学'], ['生物'], ['英语'], ['化学'], ['数学'], ['物理'], ['语文']],
+        [['早读'], ['化学'], ['物理'], ['英语'], ['语文'], ['语文'], ['生物'], ['化学'], ['数学'], ['英语'], ['生物'], ['数学'], ['物理'], ['语文']],
+        [['早读'], ['生物'], ['数学'], ['数学'], ['语文'], ['英语'], ['物理'], ['生物'], ['化学'], ['英语'], ['化学'], ['数学'], ['物理'], ['语文']],
+        [['早读'], ['化学'], ['语文'], ['英语'], ['数学'], ['数学'], ['化学'], ['物理'], ['生物'], ['英语'], ['生物'], ['数学'], ['物理'], ['语文']],
+        [['早读'], ['生物'], ['数学'], ['物理'], ['语文'], ['英语'], ['化学'], ['生物'], ['英语'], ['英语'], ['化学'], ['数学'], ['物理'], ['语文']],
+        [['早读'], ['化学'], ['生物'], ['生物'], ['英语'], ['英语'], ['数学'], ['数学'], ['数学'], ['数学'], ['生物'], ['数学'], ['物理'], ['语文']]
+    ],//五班
+    [],//六班
 ];
-let gapName = ["大吃特吃","课间","课间","大课间","课间","大吃特吃+午休","课间","课间","课间","课间","大吃特吃","课间","课间","放学"];
+let gapName = ["大吃特吃", "课间", "课间", "大课间", "课间", "大吃特吃+午休", "课间", "课间", "课间", "课间", "大吃特吃", "课间", "课间", "放学"];
 let classListHeadTemplate = "<th>%DAY%</th>";
 let classListBodyTemplate = "<tr><td>%CLASS_NAME%</td></tr>";
 
-init();
+classListInit();
 
-function init(){
+function classListInit() {
+    getCurrentClassClassList(getUrlParam("class"));
     changeClassList();
     changeTimePost();
     setInterval(changeClassList, 600000);
     setInterval(changeTimePost, 1000);
     return true;
+}
+
+function getCurrentClassClassList(classID) {
+    console.log(classID);
+    if (classID == null) classList = classList[4];
+    else classList = classList[parseInt(classID) - 1];
+}
+
+function getUrlParam(name) {
+    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    let r = window.location.search.substr(1).match(reg);  //匹配目标参数
+    if (r != null) return unescape(r[2]);
+    return null; //返回参数值
 }
 
 function getWeekDate() {
@@ -33,7 +55,7 @@ function getWeekDate() {
     return weeks[day];
 }
 
-function changeClassList(){
+function changeClassList() {
     $(".class-list thead tr").html(classListHeadTemplate.replace("%DAY%", getWeekDate));
     let now = new Date();
     let day = now.getDay();
@@ -44,11 +66,10 @@ function changeClassList(){
     }
 }
 
-function changeTimePost(){
+function changeTimePost() {
     let onClassMark = false;
     let now = new Date();
-    let min = now.getHours() * 60 + now.getMinutes();
-
+    let min = now.getHours() * 60 + now.getMinutes() + timePreFix;
     for (let i = 0; i < classStartSchedule.length; i++) {
         if (classStartSchedule[i][0] * 60 + classStartSchedule[i][1] <= min &&
             classEndSchedule[i][0] * 60 + classEndSchedule[i][1] > min) {
@@ -61,10 +82,6 @@ function changeTimePost(){
         }
     }
     if (!onClassMark) {
-        // $(".going-class ").html("无");
-        // $(".start-time").html("无");
-        // $(".end-time").html("无");
-        // $(".process-bar .layui-progress-bar").attr("lay-percent", "1/1")
         let offClassMark = false;
         for (let i = 0; i < classStartSchedule.length - 1; i++) {
             let endTime = classStartSchedule[i + 1][0] * 60 + classStartSchedule[i + 1][1];
@@ -78,7 +95,7 @@ function changeTimePost(){
                 break;
             }
         }
-        if (!offClassMark){
+        if (!offClassMark) {
             if (now > classEndSchedule[classEndSchedule.length - 1][0] * 60 + classEndSchedule[classEndSchedule.length - 1][1]) {
                 $(".going-class ").html("放学");
                 $(".start-time").html("21:50");
---
+++ js/clockController.js
@@ -0,0 +1,20 @@
+clockInit();
+function clockInit() {
+    setInterval(changeClockTime, 1000);
+}
+
+function changeClockTime() {
+    let now = new Date();
+    let gaoKaoTime = new Date('2022-06-07 09:00:00');
+    // console.log(now);
+    // console.log(gaoKaoTime);
+    let clock = gaoKaoTime.getTime()-now.getTime();
+    let clockDay = $(".clock-day span");
+    let clockHour = $(".clock-hour span");
+    let clockMinute = $(".clock-minute span");
+    let clockSecond = $(".clock-second span");
+    clockDay.html(Math.floor(clock / 86400000));
+    clockHour.html(Math.floor((clock / 1440000) % 24));
+    clockMinute.html(Math.floor((clock / 3600000) % 60));
+    clockSecond.html(Math.floor((clock / 1000) % 60));
+}
