c5aac1a98c06f5f83fee684390c031beac0e81f0
EnderTheCoder <1991455223@qq.com>
2021-09-27 23:19:10 +0800

    课程表修改为从数据库获取

--- js/classListController.js
+++ js/classListController.js
@@ -6,28 +6,19 @@ let classEndSchedule =
 let classIntervalName = ["早一", "早二", "上一", "上二", "上三", "上四", "下一", "下二", "下三", "下四", "课活", "晚一", "晚二", "晚三"];
 
 let classList = [
-    [],//一班
-    [],//二班
-    [],//三班
-    [
-        [['早读'], ['自习'], ['英语'], ['英语'], ['物理'], ['物理'], ['语文'], ['语文'], ['新闻周刊'], ['放荡不羁'], ['放荡不羁'], ['自习'], ['自习'], ['自习']],
-        [['早读'], ['生物'], ['语文'], ['化学'], ['数学'], ['数学'], ['生物'], ['物理'], ['英语'], ['英语'], ['化学'], ['数学'], ['物理'], ['语文']],
-        [['早读'], ['化学'], ['语文'], ['语文'], ['生物'], ['英语'], ['化学'], ['数学'], ['物理'], ['英语'], ['生物'], ['数学'], ['物理'], ['语文']],
-        [['早读'], ['生物'], ['英语'], ['化学'], ['物理'], ['生物'], ['语文'], ['数学'], ['化学'], ['英语'], ['化学'], ['数学'], ['物理'], ['语文']],
-        [['早读'], ['化学'], ['数学'], ['数学'], ['英语'], ['语文'], ['生物'], ['物理'], ['化学'], ['英语'], ['生物'], ['数学'], ['物理'], ['语文']],
-        [['早读'], ['生物'], ['物理'], ['英语'], ['数学'], ['语文'], ['化学'], ['生物'], ['英语'], ['英语'], ['化学'], ['数学'], ['物理'], ['语文']],
-        [['早读'], ['化学'], ['化学'], ['化学'], ['生物'], ['生物'], ['数学'], ['数学'], ['数学'], ['数学'], ['生物'], ['数学'], ['物理'], ['语文']],
-    ],//四班
-    [
-        [['早读'], ['自习'], ['英语'], ['英语'], ['物理'], ['物理'], ['语文'], ['语文'], ['新闻周刊'], ['放荡不羁'], ['放荡不羁'], ['自习'], ['自习'], ['自习']],
-        [['早读'], ['化/生'], ['数学'], ['数学'], ['物理'], ['语文'], ['英语'], ['生物'], ['化学'], ['英语'], ['化学'], ['数学'], ['物理'], ['语文']],
-        [['早读'], ['化/生'], ['物理'], ['英语'], ['语文'], ['语文'], ['化学'], ['数学'], ['生物'], ['英语'], ['化学'], ['数学'], ['物理'], ['语文']],
-        [['早读'], ['化/生'], ['化学'], ['物理'], ['生物'], ['英语'], ['数学'], ['语文'], ['体育'], ['英语'], ['化学'], ['数学'], ['物理'], ['语文']],
-        [['早读'], ['化/生'], ['英语'], ['语文'], ['数学'], ['数学'], ['化学'], ['物理'], ['生物'], ['英语'], ['化学'], ['数学'], ['物理'], ['语文']],
-        [['早读'], ['化/生'], ['数学'], ['语文'], ['英语'], ['英语'], ['物理'], ['化学'], ['生物'], ['英语'], ['化学'], ['数学'], ['物理'], ['语文']],
-        [['早读'], ['化/生'], ['生物'], ['生物'], ['化学'], ['化学'], ['数学'], ['数学'], ['数学'], ['数学'], ['化学'], ['数学'], ['物理'], ['语文']]
-    ],//五班
-    [],//六班
+    ["无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入"],
+
+    ["无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入"],
+
+    ["无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入"],
+
+    ["无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入"],
+
+    ["无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入"],
+
+    ["无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入"],
+
+    ["无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入","无数据请录入"],
 ];
 let gapName = ["大吃特吃", "课间", "课间", "大课间", "课间", "大吃特吃+午休", "课间", "课间", "课间", "课间", "大吃特吃", "课间", "课间", "放学"];
 let classListHeadTemplate = "<th>时间</th><th>%DAY%</th>";
@@ -46,8 +37,27 @@ function classListInit() {
 
 function getCurrentClassClassList(classID) {
     console.log(classID);
-    if (classID == null) classList = classList[4];
-    else classList = classList[parseInt(classID) - 1];
+    if (classID === null) return;
+    let postData = {};
+    postData.class = classID;
+    $.ajax({
+        //请求方式
+        type : "POST",
+        data: postData,
+        dataType: 'json',
+        url : "api/module/getClassData.php",
+        success : function(result) {
+            if (result.code === 200) {
+                classList = Object.values(result);
+                classList = classList["class_list"];
+                console.log("class_list");
+            } else console.log(result);
+        },
+        error : function(e){
+            console.log(e.status);
+            console.log(e.responseText);
+        }
+    });
 }
 
 function getUrlParam(name) {
@@ -71,7 +81,7 @@ function changeClassList() {
     let body = $(".class-list tbody");
     body.html("");
     for (let i = 0; i < classList[day].length; i++) {
-        body.append((classListBodyTemplate.replace("%CLASS_NAME%", classList[day][i][0])).replace("%CLASS_INTERVAL_NAME%", classIntervalName[i]));
+        body.append((classListBodyTemplate.replace("%CLASS_NAME%", classList[day][i])).replace("%CLASS_INTERVAL_NAME%", classIntervalName[i]));
     }
 }
 
@@ -89,7 +99,6 @@ function changeTimePost() {
             $(".going-class").html(classList[now.getDay()][i][0]);
             $(".start-time").html("" + addZeroToTime(classStartSchedule[i][0]) + ":" + addZeroToTime(classStartSchedule[i][1]));
             $(".end-time").html("" + addZeroToTime(classEndSchedule[i][0]) + ":" + addZeroToTime(classEndSchedule[i][1]));
-            // $(".process-bar .layui-progress-bar").attr("lay-percent", "" + (min - classStartSchedule[i][0] * 60 - classStartSchedule[i][1]) + "/" + (classEndSchedule[i][0] * 60 + classEndSchedule[i][1] - (classStartSchedule[i][0] * 60 + classStartSchedule[i][1])))
             classProgress = ((min - classStartSchedule[i][0] * 60 - classStartSchedule[i][1]) / (classEndSchedule[i][0] * 60 + classEndSchedule[i][1] - (classStartSchedule[i][0] * 60 + classStartSchedule[i][1])));
             onClassMark = true;
 
